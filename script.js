43ac425772453e0bb57b560bfab31d4d7e91c266
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-09-03 17:12:13 +0100

    declare var and log into console

---
+++ script.js
@@ -0,0 +1,9 @@
+// Declare variables using var
+var personName = "Sakura";
+var pets = 3;
+var funFact = "I like pineapple on my pizza.";
+
+// Use + to combine data and variable names in a single console log
+console.log("My name is " + personName + ".");
+console.log("I have " + pets + " pet(s).");
+console.log("Fun fact: " + funFact);
