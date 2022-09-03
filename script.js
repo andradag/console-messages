c7a483820cc0cbae5bb1246e978481827531a58b
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-09-03 17:13:17 +0100

    re-assign var

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ var funFact = "I like pineapple on my pizza.";
 console.log("My name is " + personName + ".");
 console.log("I have " + pets + " pet(s).");
 console.log("Fun fact: " + funFact);
+
+// When re-assigning variables, use variable name
+personName = "Mateo";
+pets = 5;
+funFact = "I was a yo-yo champ in third grade.";
+
+// Logs message with re-assigned values
+console.log("My name is " + personName + ".");
+console.log("I have " + pets + " pet(s).");
+console.log("Fun fact: " + funFact);
